057c8a88fe2e6f4c75df6e2fd31a28e26f500a66
Natanael Melo <109748470+Natanael135@users.noreply.github.com>
2025-03-06 12:51:25 -0300

    add: adicionando novo projeto a lista

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -15,14 +15,14 @@ const projectsData = [
     codeUrl: '',
   },
   {
-    id: 1,
-    title: 'Horario das Topiques',
-    description: 'Projeto para populção consultar horarios dos transportes alternativos na regiao de Sobral-CE.',
-    image: '/images/topiqueProject.png',
-    tags: ['HTML5', 'CSS3', 'JavaScript', 'react.js', 
-    'node.js', 'express.js', 'MongoDB', 'Styled Components', 'Netlify'],
-    demoUrl: 'https://topicmorrinhos.netlify.app/',
-    codeUrl: '',
+    id: 2,
+    title: 'To-Do List',
+    description: 'Projeto para gerenciamento de tarefas.',
+    image: '/images/todolistProjet.png',
+    tags: ['HTML5', 'CSS3', 'JavaScript', 'React.js', 
+     'Styled Components', 'Vercel'],
+    demoUrl: 'https://to-do-list-tau-pied-66.vercel.app/',
+    codeUrl: 'https://github.com/Natanael135/To-Do-List',
   },
 
 ];
