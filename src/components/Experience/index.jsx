e9a248633e5af5c4f96a42131b18844613d5c848
Natanael Melo <109748470+Natanael135@users.noreply.github.com>
2025-03-06 18:23:03 -0300

    style: melhorando identação

--- src/components/Experience/index.jsx
+++ src/components/Experience/index.jsx
@@ -1,56 +1,72 @@
-import {ExperienceContainer,ExperienceCard, CardHeader, 
-    ExperienceTitle, ExperienceDate,ExperienceDescription, 
-     IconContainer,Timeline,CardTitle, ExperienceCompanyName, TimelineItem}  from './Experience.styles';
-
+import {
+  ExperienceContainer,
+  ExperienceCard,
+  CardHeader,
+  ExperienceTitle,
+  ExperienceDate,
+  ExperienceDescription,
+  IconContainer,
+  Timeline,
+  CardTitle,
+  ExperienceCompanyName,
+  TimelineItem,
+} from "./Experience.styles";
 
 export const ExperienceSection = () => {
   return (
-    <ExperienceContainer id='experience'>
-      <ExperienceCard >
+    <ExperienceContainer id="experience">
+      <ExperienceCard>
         <CardHeader>
           <IconContainer>
-            <i className='bx bx-briefcase'></i>
+            <i className="bx bx-briefcase"></i>
           </IconContainer>
           <CardTitle>Experiência</CardTitle>
         </CardHeader>
 
         <Timeline>
-
-        <TimelineItem>
+          <TimelineItem>
             <ExperienceTitle>Desenvolvedor Front-end Júnior</ExperienceTitle>
             <ExperienceCompanyName>Forja</ExperienceCompanyName>
             <ExperienceDate>Fev/2024 - Presente</ExperienceDate>
-            <ExperienceDescription>
-              Em Breve
-            </ExperienceDescription>
+            <ExperienceDescription>Em Breve</ExperienceDescription>
           </TimelineItem>
 
           <TimelineItem>
-            <ExperienceTitle>Desenvolvedor Front-end Freelancer</ExperienceTitle>
+            <ExperienceTitle>
+              Desenvolvedor Front-end Freelancer
+            </ExperienceTitle>
             <ExperienceCompanyName>iFestify</ExperienceCompanyName>
             <ExperienceDate>Nov/2024 - Dez/2024</ExperienceDate>
             <ExperienceDescription>
-            <ul>
-            <li>Construção de uma Landing Page responsiva e otimizada.</li>
-    <li>Organização de rotas para navegação fluida entre páginas, com foco no futuro escalonamento.</li>
-    <li>Implementação de estratégias de SEO para melhor ranqueamento.</li>
-    <li>Utilização de tecnologias como HTML, CSS, JavaScript e React.js.</li>
-  </ul>
+              <ul>
+                <li>Construção de uma Landing Page responsiva e otimizada.</li>
+                <li>
+                  Organização de rotas para navegação fluida entre páginas, com
+                  foco no futuro escalonamento.
+                </li>
+                <li>
+                  Implementação de estratégias de SEO para melhor ranqueamento.
+                </li>
+                <li>
+                  Utilização de tecnologias como HTML, CSS, JavaScript e
+                  React.js.
+                </li>
+              </ul>
             </ExperienceDescription>
           </TimelineItem>
-
-
 
           <TimelineItem>
             <ExperienceTitle>Projetos Pessoais</ExperienceTitle>
             {/*<ExperienceCompanyName>Pessoal</ExperienceCompanyName>*/}
             <ExperienceDate>2023 - Presente</ExperienceDate>
             <ExperienceDescription>
-            Desenvolvimento de projetos próprios, buscando aprimorar habilidades técnicas e construir um portfólio diversificado. Através desses projetos, explorando novas tecnologias, práticas de desenvolvimento e design, com o objetivo de expandir a experiência profissional e atuação no mercado.
+              Desenvolvimento de projetos próprios, buscando aprimorar
+              habilidades técnicas e construir um portfólio diversificado.
+              Através desses projetos, explorando novas tecnologias, práticas de
+              desenvolvimento e design, com o objetivo de expandir a experiência
+              profissional e atuação no mercado.
             </ExperienceDescription>
           </TimelineItem>
-
-
         </Timeline>
       </ExperienceCard>
     </ExperienceContainer>
