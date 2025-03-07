6f4adafdc830d58263abf2d995706085fbdbc5e1
Natanael Melo <109748470+Natanael135@users.noreply.github.com>
2025-03-07 10:45:20 -0300

    style: removendo padding.

--- src/components/Skills/Skills.styles.js
+++ src/components/Skills/Skills.styles.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 import { theme } from '../../styles/theme';
 
 export const SkillsContainer = styled.section`
-  padding: ${theme.spacing.section} 0;
   background: ${theme.colors.background};
   margin-bottom: 2rem;
 `;
