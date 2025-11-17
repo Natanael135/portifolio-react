a811c859c4d64e29de2a2ae1d4088801ea156d34
Natanael Melo <natanaelmelo.dev@gmail.com>
2025-11-17 11:35:42 -0300

    fix: corrigindo datas

--- src/components/Experience/index.jsx
+++ src/components/Experience/index.jsx
@@ -26,22 +26,31 @@ export const ExperienceSection = () => {
 
           <Timeline>
             <TimelineItem>
-              <ExperienceTitle>Desenvolvedor Fullstack Júnior</ExperienceTitle>
-              <ExperienceCompanyName>Forja</ExperienceCompanyName>
-              <ExperienceDate>Fev/2024 - Presente</ExperienceDate>
+              <ExperienceTitle>Desenvolvedor Fullstack</ExperienceTitle>
+              <ExperienceCompanyName>Cash.AI</ExperienceCompanyName>
+              <ExperienceDate>Ago/2025 - Presente</ExperienceDate>
               <ExperienceDescription>
-                <p>Em Breve...</p>
+                <p>
+                  Desenvolvi o Cash.AI, uma ferramenta inovadora de finanças
+                  pessoais que utiliza IA para controlar gastos e metas através
+                  de conversas no WhatsApp. Implementei funcionalidades de
+                  categorização automática de despesas, geração de relatórios
+                  claros e planejamento financeiro inteligente, utilizando
+                  tecnologias como Nest, PostgreSQL e React.
+                </p>
               </ExperienceDescription>
             </TimelineItem>
 
             <TimelineItem>
-              <ExperienceTitle>
-                Desenvolvedor Fullstack Voluntário
-              </ExperienceTitle>
-              <ExperienceCompanyName>ONG Recomeçando</ExperienceCompanyName>
-              <ExperienceDate>Jan/2024 - Presente</ExperienceDate>
+              <ExperienceTitle>Desenvolvedor Fullstack Júnior</ExperienceTitle>
+              <ExperienceCompanyName>Forja</ExperienceCompanyName>
+              <ExperienceDate>Fev/2025 - Jul/2025</ExperienceDate>
               <ExperienceDescription>
-                <p>Em Breve...</p>
+                <p>
+                  Atuei como Desenvolvedor Fullstack Júnior, desenvolvendo
+                  aplicações web completas utilizando tecnologias front-end e
+                  back-end para entregar soluções eficientes e escaláveis.
+                </p>
               </ExperienceDescription>
             </TimelineItem>
 
@@ -93,11 +102,19 @@ export const ExperienceSection = () => {
               </ExperienceDescription>
             </TimelineItem>
             <TimelineItem>
+              <ExperienceTitle>Sistemas de Informação</ExperienceTitle>
+              <ExperienceCompanyName>Uniter</ExperienceCompanyName>
+              <ExperienceDate>Jun/2025 - Presente</ExperienceDate>
+              <ExperienceDescription>
+                <p>Em Breve...</p>
+              </ExperienceDescription>
+            </TimelineItem>
+            <TimelineItem>
               <ExperienceTitle>Desenvolvimento Full Stack</ExperienceTitle>
               <ExperienceCompanyName>
                 Capacita Brasil | UECE | Instituto Atlântico
               </ExperienceCompanyName>
-              <ExperienceDate>Jul/2024 - Atualmente</ExperienceDate>
+              <ExperienceDate>Jul/2024 - Ago/2025</ExperienceDate>
               <ExperienceDescription>
                 <p>Tecnologias Ultilizadas:</p>
 
